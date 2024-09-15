7cf4def25d376f596695974132eb490764aa62ec
Abdul Wahab <121871526+abdulwahab-aka-proton@users.noreply.github.com>
2024-09-16 01:59:02 +0500

    Update redditbot.py
    Made some edits in the code.

--- redditbot.py
+++ redditbot.py
@@ -1,92 +1,15 @@
 import praw
 from urllib.parse import urlparse, parse_qs
-import configparser
-import os
-
-"""CONFIG_FILE = "User_profiles.ini"
-
-def save_credentials(username, password, client_id, client_secret, user_agent, profile_name):
-    config = configparser.ConfigParser()
-
-    config.read(CONFIG_FILE)
-
-    if "Profiles" not in config:
-        config.add_section("Profiles")
-
-    config[profile_name] = {
-        "username": username,
-        "password": password,
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "user_agent": user_agent
-    }
-
-    config["CurrentProfile"] = {"name": profile_name}
-
-    with open(CONFIG_FILE, "w") as configfile:
-        config.write(configfile)
-
-def load_profiles():
-    config = configparser.ConfigParser()
-    config.read(CONFIG_FILE)
-
-    if "Profiles" in config:
-        return list(config["Profiles"].keys())
-    else:
-        return []
-    
-def authenticate_profile(profile_name):
-    profiles = load_profiles()
-
-    if profiles and profile_name in profiles:
-        credentials = profiles[profile_name]
-        reddit = praw.Reddit(
-            client_id = credentials["client_id"],
-            client_secret = credentials["client_secret"],
-            password = credentials["password"],
-            user_agent = credentials["user_agent"],
-            username = credentials["username"],
-        )
-        return reddit
-    else: 
-        return None
-    
-def remove_profile(profile_name):
-    config = configparser.ConfigParser()
-    config.read(CONFIG_FILE)
-
-    if "Profiles" in config and profile_name in config["Profiles"]:
-        config.remove_option("Profiles", profile_name)
-
-        with open(CONFIG_FILE, "w") as configfile:
-            config.write(configfile)
-
-def get_current_profile():
-    config = configparser.ConfigParser()
-    config.read(CONFIG_FILE)
-
-    if "CurrentProfile" in config and "name" in config["CurrentProfile"]:
-        return config["CurrentProfile"]["name"]
-    else:
-        return None"""
     
 def terminator():
-    """config = configparser.ConfigParser()
-    config.read(CONFIG_FILE)
-    config.remove_section("Profiles")
-    with open(CONFIG_FILE, "w") as configfile:
-        config.write(configfile)
-    os.remove(CONFIG_FILE)"""
-    #print("All profiles and and credentials deleted. Exiting.")
     print("Process Terminated!")
     exit()
     
-#profile_name = input("Write a name for your Profile: ")
 cl_user = input("Enter username: ")
 cl_pass = input("Enter password: ")
 cl_id = input("Enter client ID: ")
 cl_secret = input("Enter client secret: ")
-cl_useragent = input("Enter User Agent ('<Console:***:1.0>' It must be unique for each Authentication): ")
+cl_useragent = input("Enter User Agent: ")
 
 reddit = praw.Reddit(
     client_id= cl_id,
@@ -96,15 +19,12 @@ reddit = praw.Reddit(
     username= cl_user,
 )
 
-#save_credentials(cl_user, cl_pass, cl_id, cl_secret, cl_useragent, profile_name)
-
 def main():
     print("1. Upvote\n" 
           "2. Downvote\n" 
           "3. Comment\n" 
           "4. Post\n"
           "5. Exit\n"
-          #"0. --PROFILE SETTING--"
           )
     index = int(input("Enter the respective index for desired function (i.e. 1 for Upvote): "))
     if index == 1:
@@ -117,55 +37,8 @@ def main():
         func_post()
     elif index == 5:
         terminator()
-    #elif index == 0:
-        #prof_setting()
     else:
         print("Please enter correct index!")
-
-"""def prof_setting():
-
-    current_profile = get_current_profile()
-    if current_profile:
-        print(f"Currently using Profile: {current_profile}")
-
-    print("1. Switch Profile\n"
-          "2. Add Profile\n"
-          "3. Remove Profile\n"
-          "4. Back\n")
-    priv_index1 = int(input("Enter the index: "))
-     
-    if priv_index1 == 1:
-        profile_name = input("Enter name of profile you want to switch to: ")
-        reddit =  authenticate_profile(profile_name)
-
-        if reddit:
-            print(f"Switched to Profile: {profile_name}")
-            main()
-        else:
-            print(f"Profile '{profile_name}' not found!")
-            main()
-    elif priv_index1 == 2:
-        profile_name = input("Enter new Profile name: ")
-        cl_user = input("Enter Username: ")
-        cl_pass = input("Enter Password: ")
-        cl_id = input("Enter Client ID: ")
-        cl_secret = input("Enter Client secret: ")
-        cl_useragent = input("Enter Useragent ('<Console:***:1.0>' It must be unique for each Authentication): ")
-
-        save_credentials(cl_user, cl_pass, cl_id, cl_secret, cl_useragent, profile_name)
-
-        print(f"Profile '{profile_name}' added!")
-        main()
-    elif priv_index1 == 3: 
-        profile_name = input("Enter name of Profile you want to remove: ")
-        remove_profile(profile_name)
-        print(f"Profile '{profile_name}' removed!")
-        main()
-    elif priv_index1 == 4:
-        main()    
-    else:
-        print("Please enter the correct index")
-        main()"""
 
 def func_upvote():
     post_url = input("Enter URL = ")
